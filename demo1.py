495827b832f78feec0879d3834d05df2e665d07e
Matko0202 <matko.krznaric@gmail.com>
2023-11-21 19:48:16 +0000

    demo.py

---
+++ demo1.py
@@ -0,0 +1,10 @@
+print ('Dobro došli u PyDev program obrazovanja')
+
+ime = 'Pero'
+prezime = 'Matić'
+datum_rodjenja = 1988
+broj_mobitela = '+385989054775'
+zaposljenje = True
+visina = 178
+
+print (ime, prezime)

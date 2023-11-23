08f78be2f56264e2915f972cfe658252bd7e3c4b
Matko0202 <matko.krznaric@gmail.com>
2023-11-23 20:15:13 +0000

    Added print demo

---
+++ print_demo.py
@@ -0,0 +1,15 @@
+# print ('5')
+
+# help (print)
+
+first_name = 'Pero'
+last_name = 'Perić'
+phone_number = '+385 98 90 54 775'
+
+#print (last_name, first_name, end=' ')
+
+#print (first_name, end = ' ')
+#print (last_name, phone_number, sep=', ')
+
+print (first_name, end = ' ')
+print (last_name, end = '\n')
